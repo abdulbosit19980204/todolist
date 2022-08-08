91c0a47e853a7bba8b111a247babf047c47408bc
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2022-08-09 04:20:47 +0500

    first commit

---
+++ go.mod
@@ -0,0 +1,26 @@
+module todolist
+
+go 1.18
+
+require (
+	github.com/gofiber/fiber/v2 v2.36.0
+	github.com/gofiber/template v1.6.30
+	github.com/lib/pq v1.10.6
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/cosmtrek/air v1.40.4 // indirect
+	github.com/creack/pty v1.1.11 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.38.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
+)
---
+++ server.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"database/sql" // add this
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq" // add this
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
+)
+
+func indexHandler(c *fiber.Ctx, db *sql.DB) error {
+	var res string
+	var todos []string
+	rows, err := db.Query("SELECT * FROM todos")
+	defer rows.Close()
+	if err != nil {
+		log.Fatalln(err)
+		c.JSON("An error occured")
+	}
+	for rows.Next() {
+		rows.Scan(&res)
+		todos = append(todos, res)
+	}
+	return c.Render("index", fiber.Map{
+		"Todos": todos,
+	})
+}
+
+type todo struct {
+	Item string
+}
+
+func postHandler(c *fiber.Ctx, db *sql.DB) error {
+	newTodo := todo{}
+	if err := c.BodyParser(&newTodo); err != nil {
+		log.Printf("An error occured: %v", err)
+		return c.SendString(err.Error())
+	}
+	fmt.Printf("%v", newTodo)
+	if newTodo.Item != "" {
+		_, err := db.Exec("INSERT into todos VALUES ($1)", newTodo.Item)
+		if err != nil {
+			log.Fatalf("An error occured while executing query: %v", err)
+		}
+	}
+
+	return c.Redirect("/")
+}
+
+func putHandler(c *fiber.Ctx, db *sql.DB) error {
+	olditem := c.Query("olditem")
+	newitem := c.Query("newitem")
+	db.Exec("UPDATE todos SET item=$1 WHERE item=$2", newitem, olditem)
+	return c.Redirect("/")
+}
+
+func deleteHandler(c *fiber.Ctx, db *sql.DB) error {
+	todoToDelete := c.Query("item")
+	db.Exec("DELETE from todos WHERE item=$1", todoToDelete)
+	return c.SendString("deleted")
+}
+
+func main() {
+	connStr := "postgresql://postgres:0204@localhost/todos?sslmode=disable"
+	// Connect to database
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return indexHandler(c, db)
+	})
+
+	app.Post("/", func(c *fiber.Ctx) error {
+		return postHandler(c, db)
+	})
+
+	app.Put("/update", func(c *fiber.Ctx) error {
+		return putHandler(c, db)
+	})
+
+	app.Delete("/delete", func(c *fiber.Ctx) error {
+		return deleteHandler(c, db)
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	app.Static("/", "./public")
+	log.Fatalln(app.Listen(fmt.Sprintf(":%v", port)))
+}
