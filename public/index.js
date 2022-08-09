8fa06cb5d436ee7cb19886eebcc9dc43f7ad3bd7
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2022-08-09 05:52:39 +0500

    modified:   public/index.js 	modified:   tmp/main.exe 	modified:   views/index.html

--- public/index.js
+++ public/index.js
@@ -1,4 +1,5 @@
 function removeFromDb(item) {
+    let input = document.getElementById(removeFromDbBtn)
     fetch(`/delete?item=${item}`, { method: "Delete" }).then(res => {
         if (res.status == 200) {
             window.location.pathname = "/"
@@ -7,7 +8,7 @@ function removeFromDb(item) {
 }
 
 function updateDb(item) {
-    let input = document.getElementById(item)
+    let input = document.getElementById(updateDbBtn)
     let newitem = input.value
     fetch(`/update?olditem=${item}&newitem=${newitem}`, { method: "PUT" }).then(res => {
         if (res.status == 200) {
